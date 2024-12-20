ad42ebe260b0d2fb6bce89dddd371b7e67db0425
Kris <109919530+maleker86@users.noreply.github.com>
2024-12-19 18:38:46 -0800

    Introduce Parameters

--- player.gd
+++ player.gd
@@ -4,12 +4,12 @@ var number = 4+5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	test()
+	addNames("Jerry","Smith")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-func test():
-	print(number)
+func addNames(first,second):
+	print(first + " " + second)
 	pass
