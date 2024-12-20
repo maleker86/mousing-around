3e21e24cb874bd8ab58a48a16bf2bf3811a00050
Kris <109919530+maleker86@users.noreply.github.com>
2024-12-19 18:54:40 -0800

    Utilize Physics_Process instead of Process

--- player.gd
+++ player.gd
@@ -1,15 +1,11 @@
 extends RigidBody2D
 
-var number = 4+5
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	addNames("Jerry","Smith")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
 	pass
-
-func addNames(first,second):
-	print(first + " " + second)
+	
+func _process(delta):
 	pass
+	
+func _physics_process(delta: float) -> void:
+	apply_force(Vector2(0,90))
