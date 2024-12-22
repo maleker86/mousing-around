f37aec7c8f1ff17fa8227f41b3b9b8557b204ddd
Kris <109919530+maleker86@users.noreply.github.com>
2024-12-21 19:01:30 -0800

    Add Input_Maps & Player Controller

--- player.gd
+++ player.gd
@@ -1,11 +1,15 @@
 extends RigidBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-	
-func _process(delta):
-	pass
-	
+var speed = 100;
+
 func _physics_process(delta: float) -> void:
-	apply_force(Vector2(0,90))
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(speed,0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-speed,0))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0,-speed))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0,speed))
+	
+	
